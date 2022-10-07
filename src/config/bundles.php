a2f92efea7a6b713a62bf32d142d244408dcf8d7
Noham <72859612+Noh4m@users.noreply.github.com>
2022-10-07 16:06:26 +0200

    fix pull

--- src/config/bundles.php
+++ src/config/bundles.php
@@ -11,4 +11,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
